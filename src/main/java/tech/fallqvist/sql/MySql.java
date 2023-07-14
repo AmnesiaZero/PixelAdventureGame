037802fd971ed6316cc8df6b98e9f65e79ae4867
andrey <andrey.cherkasov93@gmail.com>
2023-07-14 13:03:38 +0400

    Initial commit

---
+++ src/main/java/tech/fallqvist/sql/MySql.java
@@ -0,0 +1,25 @@
+package tech.fallqvist.sql;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class MySql {
+    public static Connection getConnection() throws SQLException, IOException {
+
+        Properties props = new Properties();
+        try(InputStream in = Files.newInputStream(Paths.get("sql.properties"))){
+            props.load(in);
+        }
+        String url = props.getProperty("url");
+        String username = props.getProperty("username");
+        String password = props.getProperty("password");
+
+        return DriverManager.getConnection(url, username, password);
+    }
+}
